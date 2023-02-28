0fe1483db03d6a5c658d94de9492cd8b50d0b718
Bhavya <bhavyasharma1401@gmail.com>
2023-02-28 10:26:26 +0530

    Delete button added Updated

--- src/Component/PostsApi.jsx
+++ src/Component/PostsApi.jsx
@@ -3,6 +3,17 @@ import ReactDeleteRow from 'react-delete-row';
 
 
 export default class PostsApi extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+        items:this.props.posts.posts
+    }
+}
+del = id => {
+    const newData = this.state.items.filter(item => item.id !== id);
+    console.log("@@22",newData)
+    this.setState({ items: newData });
+}
     
   render() {
     const items = this.props.posts.posts;
@@ -20,10 +31,14 @@ export default class PostsApi extends PureComponent {
 
         </thead>
         <tbody>
-      {
+        {
+        this.state.items && this.state.items.map(item =><tr><td>{item.id}</td><td>{item.title}</td><td>{item.body}</td><td>{item.tags}</td><button onClick={()=>this.del(item.id)}>Delete</button></tr>)
+        }
+
+      {/* {
           items && items.map(item => <ReactDeleteRow key ={item.id} data = {items} deleteElement= {<i>Delete</i>} iconClassName='text-danger' onDelete={ item => { return window.confirm(`Are you sure you want to delete`) }}><td>{item.id}</td><td>{item.title}</td><td>{item.body}</td><td>{item.tags}</td></ReactDeleteRow>)
 
-        }
+        } */}
     </tbody></table>
     </div>)
    
--- src/Component/TodoApi.jsx
+++ src/Component/TodoApi.jsx
@@ -3,6 +3,17 @@ import ReactDeleteRow from 'react-delete-row';
 
 
 export default class TodoApi extends PureComponent {
+    constructor(props){
+        super(props)
+        this.state = {
+            items:this.props.todos.todos
+        }
+    }
+    del = id => {
+        const newData = this.state.items.filter(item => item.id !== id);
+        console.log("@@22",newData)
+        this.setState({ items: newData });
+    }
     
   render() {
     const items = this.props.todos.todos;
@@ -18,7 +29,9 @@ export default class TodoApi extends PureComponent {
         </thead>
         <tbody>
       {
-            items && items.map(item => <ReactDeleteRow key ={item.id} data = {items} deleteElement= {<i>Delete</i>} iconClassName='text-danger' onDelete={ item => { return window.confirm(`Are you sure you want to delete`) }}><td>{item.id}</td><td>{item.todo}</td><td>{item.userId}</td></ReactDeleteRow>)
+            this.state.items && this.state.items.map(item =><tr><td>{item.id}</td><td>{item.todo}</td><td>{item.userId}</td><button onClick={()=>this.del(item.id)}>Delete</button></tr>)
+
+            // items && items.map(item => <ReactDeleteRow key ={item.id} data = {items} deleteElement= {<i>Delete</i>} iconClassName='text-danger' onDelete={ item => { return window.confirm(`Are you sure you want to delete`) }}><td>{item.id}</td><td>{item.todo}</td><td>{item.userId}</td></ReactDeleteRow>)
         }
     </tbody></table>
     </div>)
